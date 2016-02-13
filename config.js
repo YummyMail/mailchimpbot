b6ce889ef12fc546e7eb4625a909f5f5252cdf3f
Markus Tacker <m@cto.hiv>
2016-02-13 20:25:22 +0100

    chore(lint) use standard for code style

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
-'use strict';
+'use strict'
 
-var nconf = require('nconf');
+var nconf = require('nconf')
 
 nconf
   .file('local', {file: 'config.local.json'})   // read local overwrite
-  .argv()                                             // Allow overwrites from command-line
-  .env()                                              // Allow overwrites from env
-  .file('default', {file: 'config.json'});      // read defaults
+  .argv()                                       // Allow overwrites from command-line
+  .env()                                        // Allow overwrites from env
+  .file('default', {file: 'config.json'})       // read defaults
 
-nconf.set('app', require('./package.json').name);
-nconf.set('version', require('./package.json').version);
+nconf.set('app', require('./package.json').name)
+nconf.set('version', require('./package.json').version)
 
-module.exports = nconf;
+module.exports = nconf
--- main.js
+++ main.js
@@ -1,70 +1,70 @@
-'use strict';
+'use strict'
 
-var _ = require('lodash');
-var Promise = require('bluebird');
-Promise.longStackTraces();
-var config = require('./config');
-var rp = require('request-promise');
-var crypto = require('crypto');
+var _ = require('lodash')
+var Promise = require('bluebird')
+Promise.longStackTraces()
+var config = require('./config')
+var rp = require('request-promise')
+var crypto = require('crypto')
 
-var bunyan = require('bunyan');
-var logging = bunyan.createLogger({name: config.get('app')});
+var bunyan = require('bunyan')
+var logging = bunyan.createLogger({name: config.get('app')})
 
-logging.info(config.get('app') + ' v' + config.get('version') + ' (Node ' + process.version + ') started');
-logging.info('--token ' + config.get('token'));
-logging.info('--mailchimp_api_token ' + config.get('mailchimp_api_token'));
+logging.info(config.get('app') + ' v' + config.get('version') + ' (Node ' + process.version + ') started')
+logging.info('--token ' + config.get('token'))
+logging.info('--mailchimp_api_token ' + config.get('mailchimp_api_token'))
 
-var apitoken = config.get('mailchimp_api_token');
-var dc = apitoken.split('-')[1];
+var apitoken = config.get('mailchimp_api_token')
+var dc = apitoken.split('-')[1]
 
 var q = function (url) {
   return {
     uri: 'https://foo:' + apitoken + '@' + dc + '.api.mailchimp.com/3.0/' + url,
     json: true
-  };
-};
+  }
+}
 
-function fetchUserInfo(email, listName) {
+function fetchUserInfo (email, listName) {
   return rp(q('lists'))
     .then(function (response) {
       return _.filter(response.lists, function (list) {
-        return list.name + ' Recipients' === listName;
-      });
+        return list.name + ' Recipients' === listName
+      })
     })
     .then(function (lists) {
       if (lists.length !== 1) {
-        return;
+        return
       }
-      var list = lists[0];
-      return rp(q('lists/' + list.id + '/members/' + crypto.createHash('md5').update(email).digest('hex')));
+      var list = lists[0]
+      return rp(q('lists/' + list.id + '/members/' + crypto.createHash('md5').update(email).digest('hex')))
     })
     .catch(function (err) {
-      logging.error('Failed fetch user info', {err: err});
-    });
+      logging.error('Failed fetch user info', {err: err})
+    })
 }
 
-var Botkit = require('botkit');
-var controller = Botkit.slackbot();
+var Botkit = require('botkit')
+var controller = Botkit.slackbot()
 var bot = controller.spawn({
   token: config.get('token')
-});
+})
 bot.startRTM(function (err) {
   if (err) {
-    throw new Error('Could not connect to Slack');
+    throw new Error('Could not connect to Slack')
   }
-});
+})
 
 controller.on('bot_message', function (bot, message) {
-  logging.info('message', message);
-  var match = message.text.match(/^<mailto:([^@]+@[^\|]+)\|[^@]+@[^>]+> subscribed to <[^\|]+\|([^>]+)>$/);
+  logging.info('message', message)
+  var match = message.text.match(/^<mailto:([^@]+@[^\|]+)\|[^@]+@[^>]+> subscribed to <[^\|]+\|([^>]+)>$/)
   if (!match) {
-    return;
+    return
   }
   fetchUserInfo(match[1], match[2])
     .then(function (member) {
       if (!member) {
-        logging.error('Failed to fetch member info for', match);
-        return;
+        logging.error('Failed to fetch member info for', match)
+        return
       }
       rp.post({
         uri: 'https://slack.com/api/chat.postMessage',
@@ -77,7 +77,7 @@ controller.on('bot_message', function (bot, message) {
         }
       })
       .catch(function (err) {
-        logging.error('Failed to post message', err);
-      });
-    });
-});
+        logging.error('Failed to post message', err)
+      })
+    })
+})
